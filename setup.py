27badf274dfb04d9c678a2e2aace85c0e2cf8326
James Kassemi <james.kassemi@close.com>
2025-01-14 13:12:09 -0500

    PyMongo >= 4.2

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(name='mongoengine',
       long_description=LONG_DESCRIPTION,
       platforms=['any'],
       classifiers=CLASSIFIERS,
-      install_requires=['pymongo>=3.7'],
+      install_requires=['pymongo>=4.2,<5.0'],
       test_suite='nose.collector',
       **extra_opts
 )
